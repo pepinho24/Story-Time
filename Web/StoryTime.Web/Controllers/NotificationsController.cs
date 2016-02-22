cf4cfa16991f17c97e0ab7d4da0b8fa3de3abfa7
pepinho24 <p_mil4ev@hotmail.com>
2016-02-23 00:45:51 +0200

    Added empty notifications controller

---
+++ Web/StoryTime.Web/Controllers/NotificationsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoryTime.Web.Controllers
+{
+    [Authorize]
+    public class NotificationsController : Controller
+    {
+        // To get a user notified when he is added to a story or it is his turn
+        // GET: Notifications
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
